775adf1c6c9d764bd4716637aa259673a68ec6bd
rikuTanide <riku@tanide.net>
2017-01-03 18:06:53 +0900

    Cloud FrontとGulp Webserverの挙動を合わせた

--- bookshelf_debug/gulpfile.js
+++ bookshelf_debug/gulpfile.js
@@ -4,21 +4,28 @@ gulp.task('webserver', function () {
     gulp.src('web')
         .pipe(webserver({
             host: 'localhost',
-            port: 8080,
+            port: 8000,
             livereload: true,
             proxies: [
-
                 {
-                    source: '/static',
-                    target: 'http://localhost:63342/'
+                    source: '/mypage/packages',
+                    target: 'http://localhost:63342/bookshelf/bookshell_client/web/mypage/packages'
+                },
+                {
+                    source: '/mypage/main.dart.js',
+                    target: 'http://localhost:63342/bookshelf/bookshell_client/web/mypage/main.dart.js'
                 },
                 {
                     source: '/mypage',
-                    target: 'http://localhost:63342/bookshelf/bookshell_client/web/mypage/'
+                    target: 'http://localhost:8080/mypage'
+                },
+                {
+                    source: '/static/',
+                    target: 'http://localhost:63342/'
                 },
                 {
                     source: '/',
-                    target: 'http://localhost:8000/'
+                    target: 'http://localhost:8080/'
                 }
             ]
         }));
