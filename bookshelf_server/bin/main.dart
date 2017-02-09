453f2ec86df39cde8953d62b836f06e0e6cef84a
rikuTanide <riku@tanide.net>
2017-02-09 21:23:54 +0900

    サーバーサイドの対応をした。

--- bookshelf_server/bin/main.dart
+++ bookshelf_server/bin/main.dart
@@ -204,7 +204,6 @@ class Handler {
         body {
             background: lightgray;
         }
-
         #my-app {
             background: white;
         }
@@ -221,17 +220,14 @@ class Handler {
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
   ga('create', 'UA-89660523-1', 'auto');
   ga('send', 'pageview');
-
 </script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
           rel="stylesheet">
 </head>
 <body>
 <my-app id="my-app">Loading...</my-app>
-
 </body>
 </html>""")
       ..close();
