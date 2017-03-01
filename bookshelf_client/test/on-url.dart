dab4783c3b9475c6684ec3a7ec9f92c8c30bd1b3
rikuTanide <riku@tanide.net>
2017-03-01 15:31:26 +0900

    URLをパースするところを書いた。

---
+++ bookshelf_client/test/on-url.dart
@@ -0,0 +1,27 @@
+import 'package:bookshelf_client/actions/url-load/action-url-load.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test("/ならTop", () {
+    var reqPar = parseURL([], "user1");
+    expect(reqPar, equals(new URLParams(top: new Top())));
+  });
+  test("/booklogs/user1/2017/1ならmyBookLogs", () {
+    var reqPar = parseURL(["booklogs", "user1", "2017", "1"], "user1");
+    expect(
+        reqPar, equals(new URLParams(myBookLogs: new MyBookLogs(2017, 1))));
+  });
+  test("/booklogs/user2/2017/1ならbookLogs", () {
+    var reqPar = parseURL(["booklogs", "user2", "2017", "1"], "user1");
+    expect(reqPar,
+        equals(new URLParams(bookLogs: new BookLogs("user2", 2017, 1))));
+  });
+  test("/stocks/user1ならstocks", () {
+    var reqPar = parseURL(["stocks", "user1"], "user1");
+    expect(reqPar, equals(new URLParams(myStocks: new MyStocks())));
+  });
+  test("/stocks/user2", () {
+    var reqPar = parseURL(["stocks", "user2"], "user1");
+    expect(reqPar, equals(new URLParams(stocks: new Stocks("user2"))));
+  });
+}
